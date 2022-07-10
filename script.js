d2e89d587e4067bd5d845ef2dec5432872248e1f
Aaveg-Tomar <tomaraaveg@gmail.com>
2022-07-10 21:06:35 +0530

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const b = document.getElementById('b');
 
 b.addEventListener('click', function onClick() {
-  b.style.backgroundColor = 'salmon';
+  b.style.backgroundColor = 'red';
   b.style.color = 'white';
 });
