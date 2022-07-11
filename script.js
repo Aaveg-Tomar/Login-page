7c8fcba195153f38c0bb6d55e27792ccc11c9051
Aaveg-Tomar <tomaraaveg@gmail.com>
2022-07-11 10:32:35 +0530

    third commit

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ b.addEventListener('click', function onClick() {
   b.style.backgroundColor = 'red';
   b.style.color = 'white';
 });
+
+
+function validate(){
+  var username = getComputedStyle('form1Example1');
+  var password = getElementById('form1Example13');
+
+  if(username == true && password == true)
+  {
+    alert("logic successfully");
+  }
+  else{
+    alert("failed");
+  }
+}
